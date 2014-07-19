aab0f9d5875595af143aa8c0e31c258c12ec9908
David Bayon <dbayon82@googlemail.com>
2014-07-19 17:59:16 +0100

    my_attr_accessor functioning correctly

--- Level3/lib/my_attr_accessor.rb
+++ Level3/lib/my_attr_accessor.rb
@@ -1,11 +1,18 @@
 class Class
-
 	def my_attr_accessor(*args)
 		args.each do |arg|
-			self.class_eval("def #{arg}; @#{arg} = #{arg}; end")
+			self.class_eval("def #{arg}; @#{arg}; end")
 			self.class_eval("def #{arg}=(val); @#{arg} = val; end")
 		end
 	end
+end
 
+class Person
+	def initialize(name, age)
+		@name = name
+		@age = age
+	end
+
+	my_attr_accessor :name, :age
 
 end
