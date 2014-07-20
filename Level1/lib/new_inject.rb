dfb429bf0cf40a222a0b9897b691a8280663ffc3
David Bayon <dbayon82@googlemail.com>
2014-07-20 12:33:10 +0100

    inject method fleshed out for various args and operators

--- Level1/lib/new_inject.rb
+++ Level1/lib/new_inject.rb
@@ -1,14 +1,33 @@
 class Array
-	def iteration_inject(sum = 0)
-		self.each { |number| sum += number }
-		sum
+
+	def iteration_inject(*args)
+		if args.size > 2
+			raise ArgumentError
+		elsif args.size < 2
+			total = args[0] || self.shift
+			sym = :+
+		else
+			raise ArgumentError if !args[1].is_a? Symbol
+			total, sym = args[0], args[1]
+		end
+
+		self.each { |item| total = total.send(sym, item) }
+		total
 	end
 
-	def recursion_inject(sum = 0)
-		until self.count == 0
-			sum += self.shift
-			sum = self.recursion_inject(sum)
+	def recursion_inject(*args)
+		if args.size > 2
+			raise ArgumentError
+		elsif args.size < 2
+			total = args[0] || self.shift
+			sym = :+
+		else args.size == 2
+			raise ArgumentError if !args[1].is_a? Symbol
+			total, sym = args[0], args[1]
 		end
-		sum
+
+		return total if self.size == 0
+		total = total.send(sym, self.shift)
+		self.recursion_inject(total, sym)
 	end
 end
--- Level1/spec/new_inject_spec.rb
+++ Level1/spec/new_inject_spec.rb
@@ -1,15 +1,68 @@
 require 'new_inject'
 
 describe Array do
+	let(:ary) { [1,2,3,4,5] }
 
-	it 'sums an array using iteration' do
-		ary = [1,2,3,4,5]
-		expect(ary.iteration_inject).to eq 15
+	context 'iteration approach' do
+		it 'sums a simple array using iteration' do
+			expect(ary.iteration_inject(0)).to eq 15
+		end
+
+		it 'sums the same array with no starting value' do
+			expect(ary.iteration_inject).to eq 15
+		end
+
+		it 'accepts a starting value as an argument' do
+			expect(ary.iteration_inject(5)).to eq 20
+		end
+		
+		it 'cannot accept three arguments' do
+			expect { ary.iteration_inject(3,5,6) }.to raise_error
+		end
+
+		it 'raises error if arg2 is not a symbol' do
+			expect { ary.iteration_inject(3,"z") }.to raise_error
+		end
+
+		it 'can can multiply as well' do
+			expect(ary.iteration_inject(1, :*)).to eq 120
+		end
+
+		it 'can add strings' do
+			str_ary = ["hello", "darkness", "my", "old", "friend"]
+			expect(str_ary.iteration_inject).to eq "hellodarknessmyoldfriend"
+		end
 	end
 
-	it 'sums an array using recursion' do
-		ary = [1,2,3,4,5]
-		expect(ary.recursion_inject).to eq 15
+	context 'recursion approach' do
+		it 'accepts a starting value as an argument' do
+			expect(ary.recursion_inject(5)).to eq 20
+		end
+
+		it 'sums a simple array using recursion' do
+			expect(ary.recursion_inject(0)).to eq 15
+		end
+
+		it 'sums the same array with no starting value' do
+			expect(ary.recursion_inject).to eq 15
+		end
+		
+		it 'cannot accept three arguments' do
+			expect { ary.recursion_inject(3,5,6) }.to raise_error
+		end
+
+		it 'raises error if arg2 is not a symbol' do
+			expect { ary.recursion_inject(3,"z") }.to raise_error
+		end
+
+		it 'can can multiply as well' do
+			expect(ary.recursion_inject(1, :*)).to eq 120
+		end
+
+		it 'can add strings' do
+			str_ary = ["hello", "darkness", "my", "old", "friend"]
+			expect(str_ary.recursion_inject).to eq "hellodarknessmyoldfriend"
+		end
 	end
 
 end
